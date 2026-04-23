aab78852002e046f346d1ed209f2ccb244ec967c
nelson <nelsonrock100@gmail.com>
2026-04-23 14:30:14 -0600

    Reparado

--- core/config.py
+++ core/config.py
@@ -5,9 +5,9 @@ Datos hardcoded del restaurante. Para cambiar estos datos, editar este archivo.
 
 BUSINESS_INFO = {
     "name": "Buffet San Patricio",
-    "ruc": "000000-0P",
+    "ruc": "001-180870-0062V",
     "address": "Nicaragua",
-    "phone": "+505 XXXX XXXX",
+    "phone": "+505 8808 5444",
     "email": "info@buffetsanpatricio.com",
     "slogan": "Tu mejor opción en buffet",
 }
